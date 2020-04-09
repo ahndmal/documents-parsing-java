3aba19d39695fc6be434286101e4bff71e339426
Andrii Maliuta <quadr988@gmail.com>
2020-04-09 20:26:42 +0300

    Gson abd Jackson addded

--- src/main/java/com/anma/json/GsonExec.java
+++ src/main/java/com/anma/json/GsonExec.java
@@ -6,11 +6,16 @@ import com.google.gson.stream.JsonReader;
 
 import java.io.*;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 
 public class GsonExec {
 
     private final static String jsonUrl = "https://api.privatbank.ua/p24api/exchange_rates?json&date=01.04.2020";
     private final static String path = "D:\\Programming\\java\\projects\\documents-parsing\\src\\resources\\json\\cats.json";
+    private final static String newFile = "D:\\Programming\\java\\projects\\documents-parsing\\src\\resources\\json\\catsFromGson.json";
 
     public static void main(String[] args) {
 
@@ -27,10 +32,16 @@ public class GsonExec {
 
             JsonReader jsonReader2 = new JsonReader(new FileReader(new File(path)));
             Cat[] cats = gson.fromJson(jsonReader2, Cat[].class);
+
             for (Cat cat: cats) {
                 System.out.println(cat.getName());
             }
 
+            String gsonString = gson.toJson(cats);
+
+            System.out.println(gsonString);
+
+            Files.write(Paths.get(newFile), gsonString.getBytes(), StandardOpenOption.CREATE);
 
         } catch (IOException e) {
             e.printStackTrace();
