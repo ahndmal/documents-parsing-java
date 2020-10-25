665d9c33bbd4c4efe09ec95ac6b6befc504da69b
Andrii Maliuta <quadr988@gmail.com>
2020-10-25 18:17:26 +0200

    PDFBox, iText

--- src/main/java/com/anma/pdf/PDFMain.java
+++ src/main/java/com/anma/pdf/PDFMain.java
@@ -5,9 +5,15 @@ import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.itextpdf.tool.xml.XMLWorkerHelper;
+import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.io.RandomAccessBuffer;
+import org.apache.pdfbox.io.RandomAccessFile;
+import org.apache.pdfbox.io.RandomAccessRead;
+import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.ImageType;
 import org.apache.pdfbox.rendering.PDFRenderer;
+import org.apache.pdfbox.text.PDFTextStripper;
 import org.apache.pdfbox.tools.imageio.ImageIOUtil;
 import org.fit.pdfdom.PDFDomTree;
 
@@ -19,16 +25,18 @@ import java.net.URL;
 
 public class PDFMain {
 
-    public static void main(String[] args) throws IOException, ParserConfigurationException {
+    public static void main(String[] args) throws IOException, ParserConfigurationException, DocumentException {
 
-        pdfToHTML("E:\\programming\\java\\projects\\documents-parsing\\src\\resources\\pdf\\Design_test_import.pdf");
+//        pdfToHTML("E:\\programming\\java\\projects\\documents-parsing\\src\\resources\\pdf\\Design_test_import.pdf");
+//        htmlToPDF("E:\\programming\\java\\projects\\documents-parsing\\src\\resources\\test-1.html");
+        PDFtoText("E:\\programming\\java\\projects\\documents-parsing\\src\\resources\\pdf\\Design_test_import.pdf");
 
     }
 
     private static void pdfToHTML(String filename) throws IOException, ParserConfigurationException {
 
         PDDocument pdf = PDDocument.load(new File(filename));
-        Writer output = new PrintWriter("E:\\programming\\java\\projects\\documents-parsing\\src\\resources\\pdf\\out\\pdf_1.html", "utf-8");
+        Writer output = new PrintWriter("E:\\programming\\java\\projects\\documents-parsing\\src\\resources\\pdf\\out\\from_pdf.html", "utf-8");
         new PDFDomTree().writeText(pdf, output);
 
         output.close();
@@ -39,7 +47,7 @@ public class PDFMain {
         Document document = new Document();
 
         PdfWriter writer = PdfWriter.getInstance(document,
-                new FileOutputStream("src/output/html.pdf"));
+                new FileOutputStream("E:\\programming\\java\\projects\\documents-parsing\\src\\resources\\pdf\\from_html.pdf"));
         document.open();
         XMLWorkerHelper.getInstance().parseXHtml(writer, document,
                 new FileInputStream(filename));
@@ -70,6 +78,23 @@ public class PDFMain {
         document.add(Image.getInstance((new URL(input))));
         document.close();
         writer.close();
+    }
+
+    private static void PDFtoText(String filename) throws IOException {
+        File file = new File(filename);
+        String parsedText;
+        PDFParser parser = new PDFParser(new RandomAccessFile(file, "r"));
+        parser.parse();
+
+        COSDocument cosDoc = parser.getDocument();
+        PDFTextStripper pdfStripper = new PDFTextStripper();
+        PDDocument pdDoc = new PDDocument(cosDoc);
+        parsedText = pdfStripper.getText(pdDoc);
+
+        PrintWriter pw = new PrintWriter("E:\\programming\\java\\projects\\documents-parsing\\src\\resources\\txt\\from_pdf.txt");
+        pw.print(parsedText);
+        pw.close();
+
     }
 
 
