4298f78f3135952c392a185cc736670d0325d35d
andrii <quadr988@gmail.com>
2023-09-08 19:33:53 +0300

    upgrade

--- src/test/java/com/anma/tika/TIkaCoreTests.java
+++ src/test/java/com/anma/tika/TIkaCoreTests.java
@@ -1,14 +1,72 @@
 package com.anma.tika;
 
+import org.apache.tika.Tika;
+import org.apache.tika.config.TikaConfig;
+import org.apache.tika.detect.Detector;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.io.TikaInputStream;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.Parser;
-import org.apache.tika.parser.image.AbstractImageParser;
 import org.apache.tika.parser.image.ImageParser;
+import org.apache.tika.sax.BodyContentHandler;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class TIkaCoreTests {
+    private final Logger LOG = LoggerFactory.getLogger(TIkaCoreTests.class);
 
     @Test
-    void parserCore() {
+    void parseToStringExample() throws IOException, SAXException, TikaException {
+        Tika tika = new Tika();
+        try (InputStream stream = TIkaCoreTests.class.getResourceAsStream("doc/tika.docx")) {
+            var res = tika.parseToString(stream);
+            System.out.println(res);
+        }
+    }
+
+    @Test
+    void parseExample() throws IOException, SAXException, TikaException {
+        AutoDetectParser parser = new AutoDetectParser();
+        BodyContentHandler handler = new BodyContentHandler();
+        Metadata metadata = new Metadata();
+        try (InputStream stream = TIkaCoreTests.class.getResourceAsStream("test.doc")) {
+            parser.parse(stream, handler, metadata);
+            var res = handler.toString();
+            System.out.println(res);
+        }
+    }
+
+    @Test
+    void coreElements() throws TikaException, IOException, SAXException {
+        TikaConfig config = new TikaConfig("tika-config.xml");
+        Detector detector = config.getDetector();
+        Parser autoDetectParser = new AutoDetectParser(config);
+    }
+
+    @Test
+    void parserCore() throws IOException, TikaException, SAXException {
         Parser parser = new ImageParser();
+
+        InputStream stream = Files.newInputStream(Path.of("src/resources/sample.jpg"));
+
+        var out = new FileOutputStream("src/resources/samplejpg-out");
+
+        ContentHandler handler = new DefaultHandler();
+
+        parser.parse(stream, handler, new Metadata(), new ParseContext());
+
+        stream.close();
     }
 }
---
+++ src/test/java/com/anma/tika/TikaDetectionTests.java
@@ -0,0 +1,63 @@
+package com.anma.tika;
+
+import org.apache.tika.config.TikaConfig;
+import org.apache.tika.detect.DefaultDetector;
+import org.apache.tika.detect.Detector;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.io.TemporaryResources;
+import org.apache.tika.io.TikaInputStream;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.mime.MediaType;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class TikaDetectionTests {
+    File[] myListOfFiles = {new File("src/resources/sample.jpg")};
+    final String IMAGE_PATH = "src/resources/sample.jpg";
+
+    @Test
+    void detectOne() throws IOException {
+        InputStream stream = Files.newInputStream(Path.of(IMAGE_PATH));
+
+        Detector detector = new DefaultDetector();
+
+//        TemporaryResources tmp = new TemporaryResources();
+//        TikaInputStream tis = TikaInputStream.get(stream, tmp, metadata);
+//        this.maybeSpool(tis, this.autoDetectParserConfig, metadata);
+//        MediaType type = this.detector.detect(tis, metadata);
+
+        detector.detect(stream, new Metadata());
+
+    }
+
+    @Test
+    void detectTwo() throws TikaException, IOException {
+        TikaConfig tika = new TikaConfig();
+
+        for (File file : myListOfFiles) {
+            Metadata metadata = new Metadata();
+            //TikaInputStream sets the TikaCoreProperties.RESOURCE_NAME_KEY
+            //when initialized with a file or path
+
+            MediaType mimetype = tika.getDetector().detect(TikaInputStream.get(file, metadata), metadata);
+            System.out.println("File " + file + " is " + mimetype);
+        }
+
+        InputStream[] streams = { new FileInputStream("src/resources/sample.jpg") };
+
+        for (InputStream is : streams) {
+            Metadata metadata = new Metadata();
+            //if you know the file name, it is a good idea to
+            //set it in the metadata, e.g.
+            //metadata.set(TikaCoreProperties.RESOURCE_NAME_KEY, "somefile.pdf");
+            String mimetype = tika.getDetector().detect(TikaInputStream.get(is), metadata).toString();
+            System.out.println("Stream " + is + " is " + mimetype);
+        }
+    }
+}
