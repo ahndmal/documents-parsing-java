1a857cdffa3ea54f320893fe0c1e8da7cf59b05b
andrii <quadr988@gmail.com>
2023-09-10 18:59:24 +0300

    type detection

--- src/main/java/com/anma/tika/LanguagesIdentify.java
+++ src/main/java/com/anma/tika/LanguagesIdentify.java
@@ -5,8 +5,8 @@ import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.parser.ParseContext;
 
 import org.apache.tika.exception.TikaException;
-import org.apache.tika.language.LanguageIdentifier;
-import org.apache.tika.language.ProfilingHandler;
+//import org.apache.tika.language.LanguageIdentifier;
+//import org.apache.tika.language.ProfilingHandler;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.DelegatingParser;
 import org.apache.tika.parser.ParseContext;
@@ -14,24 +14,24 @@ import org.apache.tika.sax.TeeContentHandler;
 
 public class LanguagesIdentify {
     void identify() {
-        LanguageProfile profile = new LanguageProfile(
-                "Alla människor är födda fria och" + " lika i värde och rättigheter.");
-        LanguageIdentifier identifier = new LanguageIdentifier(profile);
-        System.out.println(identifier.getLanguage());
-
-        ProfilingWriter writer = new ProfilingWriter();
-        writer.append("Minden emberi lény");
-        writer.append(" szabadon születik és");
-        writer.append(" egyenl? méltósága és");
-        writer.append(" joga van.");
-        LanguageIdentifier identifier =
-                writer.getLanguage();
-        System.out.println(identifier.getLanguage());
-
-
-        ProfilingHandler handler = new ProfilingHandler();
-        new AutoDetectParser().parse(System.in, handler, new Metadata(), new ParseContext());
-        LanguageIdentifier identifier = handler.getLanguage();
-        System.out.println(identifier.getLanguage());
+//        LanguageProfile profile = new LanguageProfile(
+//                "Alla människor är födda fria och" + " lika i värde och rättigheter.");
+//        LanguageIdentifier identifier = new LanguageIdentifier(profile);
+//        System.out.println(identifier.getLanguage());
+//
+//        ProfilingWriter writer = new ProfilingWriter();
+//        writer.append("Minden emberi lény");
+//        writer.append(" szabadon születik és");
+//        writer.append(" egyenl? méltósága és");
+//        writer.append(" joga van.");
+//        LanguageIdentifier identifier =
+//                writer.getLanguage();
+//        System.out.println(identifier.getLanguage());
+//
+//
+//        ProfilingHandler handler = new ProfilingHandler();
+//        new AutoDetectParser().parse(System.in, handler, new Metadata(), new ParseContext());
+//        LanguageIdentifier identifier = handler.getLanguage();
+//        System.out.println(identifier.getLanguage());
     }
 }
--- src/test/java/com/anma/tika/pdf/TikaPdfParsingTest.java
+++ src/test/java/com/anma/tika/pdf/TikaPdfParsingTest.java
@@ -1,42 +1,125 @@
 package com.anma.tika.pdf;
 
-import org.apache.tika.Tika;
 import org.apache.tika.exception.TikaException;
+import org.apache.tika.extractor.EmbeddedDocumentExtractor;
 import org.apache.tika.io.TikaInputStream;
 import org.apache.tika.metadata.Metadata;
+import org.apache.tika.metadata.PDF;
+import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.html.HtmlMapper;
 import org.apache.tika.parser.html.IdentityHtmlMapper;
+import org.apache.tika.parser.image.ImageParser;
 import org.apache.tika.parser.pdf.PDFParser;
-import org.apache.tika.sax.BodyContentHandler;
+import org.apache.tika.parser.pdf.PDFParserConfig;
 import org.apache.tika.sax.ToHTMLContentHandler;
+import org.apache.tika.sax.ToXMLContentHandler;
+import org.apache.tika.sax.XHTMLContentHandler;
+import org.apache.tika.sax.xpath.Matcher;
+import org.apache.tika.sax.xpath.MatchingContentHandler;
+import org.apache.tika.sax.xpath.XPathParser;
 import org.junit.jupiter.api.Test;
+import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.URI;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class TikaPdfParsingTest {
     private static final String PDF_FILE = "/home/andrii/Documents/pdf/Site_FS.pdf";
 
     @Test
+    void ExtractImages() {
+        String xhtmlContents = "";
+        AutoDetectParser parser = new AutoDetectParser();
+        ContentHandler handler = new ToXMLContentHandler();
+        Metadata metadata = new Metadata();
+        ParseContext context = new ParseContext();
+        EmbeddedDocumentExtractor ede = new EmbeddedDocumentExtractor() {
+            @Override
+            public boolean shouldParseEmbedded(Metadata metadata) {
+                return true;
+            }
+
+            @Override
+            public void parseEmbedded(InputStream stream, ContentHandler handler, Metadata metadata, boolean outputHtml) throws IOException {
+                Path outputDir = Path.of(".");
+                Files.createDirectories(outputDir);
+
+                System.out.println(">>> metadata");
+                System.out.println(metadata.toString());
+
+//                Path outputPath = new File(outputDir.toString() + "/" + metadata.get(Metadata.RESOURCE_NAME_KEY)).toPath();
+                Path outputPath = new File(outputDir.toString() + "/" + metadata.get("resourceName")).toPath();
+                Files.deleteIfExists(outputPath);
+                Files.copy(stream, outputPath);
+            }
+        };
+
+        PDFParserConfig pdfConfig = new PDFParserConfig();
+        pdfConfig.setExtractInlineImages(true);
+        pdfConfig.setExtractUniqueInlineImagesOnly(true);
+
+        context.set(PDFParserConfig.class, pdfConfig);
+        context.set(EmbeddedDocumentExtractor.class, ede);
+        context.set(AutoDetectParser.class, parser);
+
+        try (InputStream stream = new FileInputStream(PDF_FILE)) {
+            parser.parse(stream, handler, metadata, context);
+            xhtmlContents = handler.toString();
+        } catch (IOException | SAXException | TikaException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(xhtmlContents);
+    }
+
+    @Test
     void parsePdf() throws IOException, TikaException, SAXException {
-        PDFParser parser = new PDFParser();
+        PDFParser pdfParser = new PDFParser();
+        AutoDetectParser parser = new AutoDetectParser();
 
         var is = TikaInputStream.get(Path.of(PDF_FILE));
 
 //        BodyContentHandler handler = new BodyContentHandler();
-          var handler = new ToHTMLContentHandler();
+        var handler = new ToHTMLContentHandler();
+        var xhtmlContentHandler = new XHTMLContentHandler(handler, new Metadata());
+//        ContentHandler handler = new ToXMLContentHandler();
 
         ParseContext context = new ParseContext();
         context.set(HtmlMapper.class, new IdentityHtmlMapper());
 
-        parser.parse(is, handler, new Metadata(), context);
+        parser.parse(is, xhtmlContentHandler, new Metadata(), context);
 
-        System.out.println(handler);
-        
+        System.out.println(xhtmlContentHandler);
+    }
+
+    @Test
+    void imageParse() throws TikaException, IOException, SAXException {
+        ImageParser imageParser = new ImageParser();
+        var handler = new ToHTMLContentHandler();
+        var is = TikaInputStream.get(Path.of(PDF_FILE));
+
+        imageParser.parse(is, handler, new Metadata(), new ParseContext());
+    }
+
+    @Test
+    void parseOnePartToHTML() throws IOException, SAXException, TikaException {
+        // Only get things under html -> body -> div (class=header)
+        XPathParser xhtmlParser = new XPathParser("xhtml", XHTMLContentHandler.XHTML);
+        Matcher divContentMatcher = xhtmlParser.parse("/xhtml:html/xhtml:body/xhtml:div/descendant::node()");
+        ContentHandler handler = new MatchingContentHandler(
+                new ToXMLContentHandler(), divContentMatcher);
+
+        AutoDetectParser parser = new AutoDetectParser();
+        Metadata metadata = new Metadata();
+        try (InputStream stream = TikaInputStream.get(Path.of(PDF_FILE))) {
+            parser.parse(stream, handler, metadata);
+            System.out.println(handler.toString());
+        }
     }
 }
